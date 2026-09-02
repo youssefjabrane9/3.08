a6d1c8102afb2314186fb7daf5f5a2116e0cb490
jabrane <jabraneyoussef136@gmailcom>
2026-09-02 11:48:58 +0200

    r309

---
+++ main.py
@@ -0,0 +1,7 @@
+def addition(a: int, b: int) -> int:
+    """
+    fonction d’addition de deux nombres entiers
+    """
+    return a if a > b else b
+def seuil(a: int, seuil : int = 100, ) ->bool:
+    return a > seuil
